0b88afb431db4467537f8142a0d0ce7bcd9a1f76
Stephen Kirkland <skirkland.job@gmail.com>
2021-07-21 02:02:54 -0400

    Started working on the Bookings section

--- src/app/tabs/home/home.component.ts
+++ src/app/tabs/home/home.component.ts
@@ -14,6 +14,7 @@ export class HomeComponent implements OnInit {
   logoHomeButton: string = '../../../assets/images/logos/KEU_Logo_FullColor_Secondary_resize.png';
   watermarkLogo: string = '../../../assets/images/logos/KEU_Logo_FullColor_Wordmark.png';
   pswLogo: string = '../../../assets/images/logos/psw_logo.png'; //https://www.coolgenerator.com/png-text-generator
+  roseBook: string = '../../../assets/images/misc/rose-book.png';
 
   constructor(private sloganSevice: SloganService) { }
 
